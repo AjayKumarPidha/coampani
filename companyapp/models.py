346ea427da91a7653940758814a80f31f398abc0
AjayKumarPidha <ajaypidha408@gmail.com>
2022-09-14 01:11:37 +0530

    add push

--- companyapp/models.py
+++ companyapp/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class school(models.Model):
+    mama=models.CharField(max_length=100,blank=True,null=True)
+    mama=models.CharField(max_length=100,blank=True,null=True)
+    mama=models.CharField(max_length=100,blank=True,null=True)
+    mama=models.CharField(max_length=100,blank=True,null=True)
    
     sama=models.CharField(max_length=100,blank=True,null=True)
     jama=models.CharField(max_length=100,blank=True,null=True)
