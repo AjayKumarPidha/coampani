9acc5d663435e078ff259c8434045b701015f13c
AjayKumarPidha <ajaypidha408@gmail.com>
2022-09-14 00:40:01 +0530

    add push

--- companyapp/models.py
+++ companyapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class school(models.Model):
+    gama=models.CharField(max_length=100,blank=True,null=True)
+    nama=models.CharField(max_length=100,blank=True,null=True)
+    fama=models.CharField(max_length=100,blank=True,null=True)
+    dama=models.CharField(max_length=100,blank=True,null=True)
