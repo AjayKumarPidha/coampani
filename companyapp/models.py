e4a8c16f0fa498773ed30c002c0a7bd5687f8456
AjayKumarPidha <ajaypidha408@gmail.com>
2022-09-14 00:45:47 +0530

    add push

--- companyapp/models.py
+++ companyapp/models.py
@@ -6,3 +6,7 @@ class school(models.Model):
     nama=models.CharField(max_length=100,blank=True,null=True)
     fama=models.CharField(max_length=100,blank=True,null=True)
     dama=models.CharField(max_length=100,blank=True,null=True)
+    sama=models.CharField(max_length=100,blank=True,null=True)
+    jama=models.CharField(max_length=100,blank=True,null=True)
+    lama=models.CharField(max_length=100,blank=True,null=True)
+    dama=models.CharField(max_length=100,blank=True,null=True)
